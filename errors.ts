48ba992e3d351e4d915fc86f987430dbd7649b59
David Pires <developer@davidpires.pt>
2024-06-17 21:55:01 +0100

    Update pipeline to reuse workflows (#2)

--- errors.ts
+++ errors.ts
@@ -3,8 +3,14 @@
  * Error class for unhandled errors.
  */
 export class UnhandledError extends Error {
+    /** Actual unhandled error */
     innerErr: unknown;
 
+    /**
+     * Creates a new instance of UnhandledError.
+     * 
+     * @param innerErr Actual unhandled error
+     */
     constructor(innerErr: unknown) {
         super('Unhandled error');
         this.innerErr = innerErr;
